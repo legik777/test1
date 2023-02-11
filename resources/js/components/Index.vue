ae953cb0cf14270a5ec9cf537bc92d03158401b7
legik777 <75944488+legik777@users.noreply.github.com>
2023-02-11 13:33:07 +0300

    first commit

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full flex-col absolute">
+    <div class="flex-col absolute">
         <div class=""><a href="#" @click.prevent="this.$router.push({name:'main'})">Главная</a></div>
         <div class=""><a href="#" @click.prevent="this.userStore.logout()">Выйти</a></div>
         <div class=""><a href="#" @click.prevent="this.$router.push({name:'user.login'})">Войти</a></div>
