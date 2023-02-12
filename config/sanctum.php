d1855801c4350c55ef99b0c3e50766f9e9f01602
legik777 <75944488+legik777@users.noreply.github.com>
2023-02-12 12:37:35 +0300

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'test.kodi,192.168.100.123,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'test.kodi,192.168.1.,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
